30d6ef8187e391fa229e6f5c7688b73bceddf509
davidsirbu <dev.sirbu@gmail.com>
2026-05-25 00:09:17 +0200

    Scrittura firma funzioni (non molto ma è un lavoro onesto)

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <postgresql/libpq-fe.h>
 
+PGconn* connetti_al_db() {
+
+}
+
+int mostra_menu() {
+
+}
+
+void esegui_e_stampa(PGconn* conn, const char* query) {
+
+}
+
 int main() {
-    char* s = "Ciao";
-    s[0] = 'M';
-    printf("%s", s);
+
 }
